78601427ec464f04fa98ddce87ab345cc7cd93a2
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-10-23 20:35:21 +0900

    add before_save on models/user.rb
    データベースに保存をするときに、大文字と小文字の区別を付けないためのメソッドの指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase } # データベースに保存するために全ての文字列を小文字に変換を行う
+  # self.email.downcaseでも良かったが、右辺は省略可能
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
