bf7151b6aa48c88cee48996c48e9b78f818e8ce7
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-10-23 16:32:47 +0900

    display minitest/reporters on test_herlper.rb
    minitestの実行結果の表示をリッチにするための変更を行った。

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+# 成功/失敗の表示設定 gem minitest-reporters
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
