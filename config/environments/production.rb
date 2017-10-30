b6195b46d96bbb2343fd8e86cf93c5958fc5093b
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-10-30 09:46:29 +0900

    add comments on environments/product.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  # ローカルサーバーからネットワークに流れる前に、情報を暗号化するため
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
