54f86391c287f7029fbca3b60a00eaaa718a6a57
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-11-06 10:34:25 +0900

    add create action and destroy action on sessions_controller.rb
    createアクションと、destroyアクションについてログイン出来るように追加。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      flash.now[:success] = "ログインしました"
+      redirect_to user # user_url(user)
+    else
+      flash.now[:danger] = "ログインできませんでした"
+      render 'new'
+    end
   end
 
-  def destory
+  def destroy
+    log_out
+    render 'new'
   end
 end
