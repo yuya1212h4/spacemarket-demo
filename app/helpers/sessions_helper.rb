fc9c47b1ff3f499235365f6a5556a64ff0635559
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-11-09 23:31:37 +0900

    change remember and current_user on sessions_helper.rb

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,9 +5,25 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # ユーザーのセッションを永続的にする
+  def rember(user)
+    user.remember
+    cookies.permanent.sigend[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   # 現在ログイン中のユーザーがいる場合に、そのユーザーを返す
+  # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if(user_id = session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
