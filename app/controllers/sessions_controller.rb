6f5f8aa6f8f3fdef3ab128b8d3227dc244c5b5c7
Yuya Tabata <yuya1212h4.066@gmail.com>
2017-11-10 09:55:05 +0900

    add rember_me method on sessoins_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       flash.now[:success] = "ログインしました"
       redirect_to user # user_url(user)
     else
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     render 'new'
   end
 end
